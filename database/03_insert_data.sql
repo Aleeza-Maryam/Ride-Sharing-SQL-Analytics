6afe7be4f2693d1e381437a9877c3015fad727cd
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-08-18 23:03:01 +0500

    inserting data

---
+++ database/03_insert_data.sql
@@ -0,0 +1,15 @@
+USE ride_sharing;
+
+INSERT INTO Customers
+(first_name, last_name, email, phone, city, signup_date)
+VALUES
+('Ali', 'Khan', 'ali.khan@gmail.com', '03001234567', 'Lahore', '2025-01-15'),
+('Sara', 'Ahmed', 'sara.ahmed@gmail.com', '03011234567', 'Islamabad', '2025-02-20'),
+('Ahmed', 'Raza', 'ahmed.raza@gmail.com', '03021234567', 'Karachi', '2025-03-10'),
+('Ayesha', 'Malik', 'ayesha.malik@gmail.com', '03031234567', 'Lahore', '2025-03-25'),
+('Hamza', 'Ali', 'hamza.ali@gmail.com', '03041234567', 'Islamabad', '2025-04-05'),
+('Zainab', 'Khan', 'zainab.khan@gmail.com', '03051234567', 'Karachi', '2025-04-18'),
+('Usman', 'Tariq', 'usman.tariq@gmail.com', '03061234567', 'Lahore', '2025-05-02'),
+('Hira', 'Aslam', 'hira.aslam@gmail.com', '03071234567', 'Rawalpindi', '2025-05-15'),
+('Bilal', 'Sheikh', 'bilal.sheikh@gmail.com', '03081234567', 'Karachi', '2025-06-01'),
+('Maham', 'Iqbal', 'maham.iqbal@gmail.com', '03091234567', 'Lahore', '2025-06-20');
