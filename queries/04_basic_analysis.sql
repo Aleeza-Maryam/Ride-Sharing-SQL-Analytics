3e627686e3282a5efa3c203d45bf81331190a8ee
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-08-19 22:56:23 +0500

    basic analysis

--- queries/04_basic_analysis.sql
+++ queries/04_basic_analysis.sql
@@ -15,5 +15,37 @@ FROM Rides;
 
 SELECT ride_status,
 COUNT(*) AS Total_rides
-FROM ride
+FROM rides
 GROUP BY ride_status;
+
+
+--Total revenue
+
+SELECT SUM(fare) AS Revenue
+FROM rides
+WHERE ride_status='Completed';
+
+SELECT
+    AVG(fare) AS average_fare
+FROM Rides
+WHERE ride_status = 'Completed';
+
+SELECT
+    AVG(distance_km) AS average_distance
+FROM Rides
+WHERE ride_status = 'Completed';
+
+
+--Which customer took which ride?
+
+SELECT
+    r.ride_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    r.ride_date,
+    r.distance_km,
+    r.fare
+FROM Rides r
+JOIN Customers c
+    ON r.customer_id = c.customer_id;
+
+    
